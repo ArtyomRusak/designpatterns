64d6dc1012459cd756d26aa7e31b0419b6909bdc
ArtsiomRusak <bembi1204@gmail.com>
2015-09-06 20:51:12 +0300

    Add Proxy design pattern.

---
+++ Learning.DesignPatterns/DesignPatterns.Structural/Proxy/BasicImplementation.cs
@@ -0,0 +1,62 @@
+﻿  using System;
+
+namespace DesignPatterns.Structural.Proxy
+{
+    /// <summary>
+    /// MainApp startup class for Structural
+    /// Proxy Design Pattern.
+    /// </summary>
+    internal class BasicMainApp
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        private static void Main()
+        {
+            // Create proxy and request a service
+            Proxy proxy = new Proxy();
+            proxy.Request();
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Subject' abstract class
+    /// </summary>
+    internal abstract class Subject
+    {
+        public abstract void Request();
+    }
+
+    /// <summary>
+    /// The 'RealSubject' class
+    /// </summary>
+    internal class RealSubject : Subject
+    {
+        public override void Request()
+        {
+            Console.WriteLine("Called RealSubject.Request()");
+        }
+    }
+
+    /// <summary>
+    /// The 'Proxy' class
+    /// </summary>
+    internal class Proxy : Subject
+    {
+        private RealSubject _realSubject;
+
+        public override void Request()
+        {
+            // Use 'lazy initialization'
+            if (_realSubject == null)
+            {
+                _realSubject = new RealSubject();
+            }
+
+            _realSubject.Request();
+        }
+    }
+}
